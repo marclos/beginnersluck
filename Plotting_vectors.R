aba5be35dc35bdc602f0f2d73d9f5945ae578962
marclos <marc.loshuertos@pomona.edu>
2016-09-07 17:34:01 -0700

    new files

---
+++ Plotting_vectors.R
@@ -0,0 +1,35 @@
+# Creating a Plot with two vectors
+# Sept 7th
+
+# First let's create vectors
+x <- c(4, 6, 39, 23, 12, 45, 12, 22)
+y <- c(1, 8, 44, 34, 23, 29, 21, 32)
+
+# What did we make?
+x
+
+#what's the first observation in x
+x[1]
+
+#third ?
+x[3]
+
+
+#fifth in y?
+y[5]
+
+plot(x, y)
+
+#if y is a function of x, y=f(x) or y ~x
+
+plot(y~x)
+
+# Let's create a best fit line -- using a linear model
+
+mymodel <- lm(y~x)
+
+# Let's extract the coefficients. 
+coef(mymodel)
+
+# Now let's plot them
+abline(coef(mymodel))
