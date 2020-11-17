8298dc8dba2e407a36d0f36d32a4ece30001bd94
marclos <marc.loshuertos@pomona.edu>
2020-11-17 14:28:32 -0800

    update shiny app

--- Climate_Reductions/Green_House_Reduction_Tool/server.R
+++ Climate_Reductions/Green_House_Reduction_Tool/server.R
@@ -12,16 +12,28 @@ library(shiny)
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
     
-    output$pergoal <- renderTable(pergoal)
-    #output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-    #    x    <- faithful[, 2]
-    #    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-    #    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
-    #})
-
+    output$distPlot <- renderTable({
+        
+        # generate annualized rate based on inputs from ui.R
+        
+        ref_reduction = input$ref*input$pergoal/100; 
+        goal=input$ref-ref_reduction; 
+        n = input$goalyear-input$refyear
+            
+        reduction=input$current-goal
+            
+        rate = round(((input$current - reduction)/input$current)^(1/n)-1,4)
+        # test fv = 1000*(1+.1)^5; fv
+        future = input$current*(1+ rate)^n
+        output = data.frame(Ref_Year=input$refyear, 
+                Goal_Year=input$goalyear, 
+                Ref_Emmissions=input$ref, 
+                Current_Emmissions=input$current, 
+                Goal=input$pergoal, 
+                Reduction_Rate=rate*100, 
+                Future_Emmissions=future)
+        print(output)
+        
+    })
+    
 })
--- Climate_Reductions/Green_House_Reduction_Tool/ui.R
+++ Climate_Reductions/Green_House_Reduction_Tool/ui.R
@@ -16,26 +16,37 @@ shinyUI(fluidPage(
     titlePanel("Green House Gas Emissions Calculator"),
     # Slider Input for Reference Year
 
-#rate.fun(refyear = 2020, goalyear= 2050, ref=5000, current= 5400, pergoal=.40)
+    HTML("<p>Emission reductions are usually developed relative to an 
+         emissions goal based on a reference year. However, we need to 
+         convert these to a annualized rate for our activity. Thus, 
+         I have created a very simple tool to convert promised reductions 
+         into annualized values.</p>"),
+
+    HTML("<p>The inputs include: 1) Reference Year -- based on a inventory 
+         year, e.g. based on 1990 emissions; 2) Goal Year -- target year to 
+         reach a reduction; 3) Reference Emissions -- amount of emmissions 
+         estimated during the reference year (Gt or billons of tons of C02 
+         emitted); 4) The current emissions (Gt), which 
+         might be above or below the reference year. Thus, taking into 
+         consideration the emissions completed so far or excesses that need 
+         to be redued; 5) What is the reduction goals as a percent, often 
+         based on Paris accord emissions reduction commitments. </p>"),
+    
+    #rate.fun(refyear = 2020, goalyear= 2050, ref=5000, current= 5400, pergoal=.40)
     # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
             sliderInput("refyear", "Reference Year", value=2020, min=1980, max=2030),
-            sliderInput("goalyear", "Goal Year", value=2050, min=2030, max=2070),
-            numericInput("ref", "Reference Emissions (Gt)", value=0, min=0, max=15, step=.1),
-            numericInput("current", "Current Emissions (Gt)", value=0, min=0, max=15, step=.1),
-            numericInput("pergoal", "Reduction goal from Reference", value=0, min=0, max=200, step=1),
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            sliderInput("goalyear", "Goal Year", value=2050, min=2020, max=2070),
+            numericInput("ref", "Reference Emissions (Gt)", value=1, min=0, max=15, step=.1),
+            numericInput("current", "Current Emissions (Gt)", value=2, min=0, max=15, step=.1),
+            numericInput("pergoal", "Percent Reduction goal from Reference", value=2, min=0, max=200, step=1)
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-            #plotOutput("distPlot")
-            tableOutput("pergoal")
+            tableOutput("distPlot")
+            # tableOutput("pergoal")
         )
     )
 ))
