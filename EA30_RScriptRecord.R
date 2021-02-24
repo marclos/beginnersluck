c3d029be91878004933aa07a5c95ad3fd2f662cb
marclos <marc.loshuertos@pomona.edu>
2021-02-24 14:15:25 -0800

    set goals

--- EA30_RScriptRecord.R
+++ EA30_RScriptRecord.R
@@ -38,20 +38,50 @@ Anscombe = as.data.frame(
     11, 5, 5, 5, 8, 5.68, 4.74, 5.73, 6.89), 
     nrow=11,  byrow=TRUE))
 
-names(Anscombe) = c("No", "x1", "x2", "x3", "x4", "y1", "y2", "y3", "y4")
+names(Anscombe) = c("Obs.", "x1", "x2", "x3", "x4", "y1", "y2", "y3", "y4")
 
-table(Anscombe)
+Anscombe
 
-plot(y1 ~ x1, Anscombe); A.lm = lm(y1 ~ x1, Anscombe)
+xmin = min(Anscombe[, 2:5]); xmin
+xmax = max(Anscombe[, 2:5]); xmax
+
+ymin = min(Anscombe[, 6:9]); ymin
+ymax = max(Anscombe[, 6:9]); ymax
+
+par(mfrow=c(2,2))
+plot(y1 ~ x1, Anscombe, main="Dataset A", xlim=c(xmin,xmax), ylim=c(ymin,ymax)); 
+  A.lm = lm(y1 ~ x1, Anscombe); abline(coef(A.lm)); 
+  text(14,5, paste("slope =", round(coef(A.lm)[2], 3)));
+  text(14,4, paste("intercept =", round(coef(A.lm)[1], 3)))
+
+plot(y2 ~ x2, Anscombe, main="Dataset B", xlim=c(xmin,xmax), ylim=c(ymin,ymax)); 
+  B.lm = lm(y2 ~ x2, Anscombe); abline(coef(B.lm))
+  text(14,6, paste("slope =", round(coef(B.lm)[2], 3)));
+  text(14,5, paste("intercept =", round(coef(B.lm)[1], 3)))
+
+plot(y3 ~ x3, Anscombe, main="Dataset C", xlim=c(xmin,xmax), ylim=c(ymin,ymax)); 
+  C.lm = lm(y3 ~ x3, Anscombe); abline(coef(C.lm))
+  text(14,6, paste("slope =", round(coef(C.lm)[2], 3)));
+  text(14,5, paste("intercept =", round(coef(C.lm)[1], 3)))
+
+  plot(y4 ~ x4, Anscombe, main="Dataset D", xlim=c(xmin,xmax), ylim=c(ymin,ymax)); 
+  D.lm = lm(y4 ~ x4, Anscombe); abline(coef(D.lm))
+  text(14,6, paste("slope =", round(coef(D.lm)[2], 3)));
+  text(14,5, paste("intercept =", round(coef(D.lm)[1], 3)))
+
+
+summary(A.lm); summary(B.lm); summary(C.lm); summary(D.lm)
+
+
 par(mfrow=c(2,2)); plot(A.lm); par(mfrow=c(1,1))
 
-plot(y2 ~ x2, Anscombe); B.lm = lm(y2 ~ x2, Anscombe)
+
 par(mfrow=c(2,2)); plot(B.lm); par(mfrow=c(1,1))
 
-plot(y3 ~ x3, Anscombe); C.lm = lm(y3 ~ x3, Anscombe)
+
 par(mfrow=c(2,2)); plot(C.lm); par(mfrow=c(1,1))
 
-plot(y4 ~ x4, Anscombe); D.lm = lm(y4 ~ x4, Anscombe)
+
 par(mfrow=c(2,2)); plot(D.lm); par(mfrow=c(1,1))
 
 summary(A.lm)
