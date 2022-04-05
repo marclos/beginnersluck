2177e4f1b298bfae5c6df124ce938789d1a59ac1
marclos <marc.loshuertos@pomona.edu>
2022-04-04 23:28:07 -0700

    update file processing

--- settler_ecology/jsonl_read_process2.R
+++ settler_ecology/jsonl_read_process2.R
@@ -1,17 +1,35 @@
 library(jsonlite)
 library(rtweet)
+library(R.utils)
+library(dplyr)
+library(tidytext)
 
-## Replace '...' with the directory that has your files
-setwd("/home/CAMPUS/mwl04747/github/beginnersluck/settler_ecology")
-files = dir(pattern = "*.jsonl$")
+## gzunip files
+setwd("/home/CAMPUS/mwl04747/github/beginnersluck/settler_ecology/data")
+files = dir(pattern = "*.gz$")
+
+#gunzip("file.gz", remove=FALSE)
+
+for (i in 1:length(files)){
+  start = Sys.time()
+  gunzip(files[i], remove=FALSE, skip=TRUE)
+  end = Sys.time()
+  message(files[i], ": ", round(difftime(end, start, units = "mins"), 2), 
+    " minutes for ", files[i], " uncompressing")
+}
+
 
 ## As many JSON fields as you want go here, for instance:
 cols = c(
   "datePublished",
+  "isPartOf",
+  "abstract",
   "fullText",
   "id"
 )
 
+files = dir(pattern = "*.jsonl$")
+## Loop for all jsonl files -> csv
 for (i in 1:length(files)){
   start = Sys.time()
   lines = readLines(files[i])
@@ -32,29 +50,8 @@ for (i in 1:length(files)){
   ## Uncomment above if you want the jsonl files to be permanently deleted (like if space is an issue)
 }
 
-BotGaz <- read.csv("4b8a40f2-fa6f-ab9c-7719-d039a3c4cc81-csv.jsonl.csv")
+#BotGaz <- read.csv("4b8a40f2-fa6f-ab9c-7719-d039a3c4cc81-csv.jsonl.csv")
 
-library(dplyr)
-library(tidytext)
 
-BotGaz %>%
-  count(fullText, sort = TRUE)
 
-sum(+(grepl("Indian", BotGaz$fullText)))
-BotGaz$Indian = +(grepl("Indian\\b", BotGaz$fullText) & !grepl("India\\b", BotGaz$fullText)); 
-sum(BotGaz$Indian)
-BotGaz$Native = +(grepl("Native", BotGaz$fullText))
-BotGaz$native = +(grepl("native", BotGaz$fullText))
-BotGaz$indigenous = +(grepl("indigenous", ignore.case = TRUE, BotGaz$fullText))
-sum(BotGaz$indigenous)
-library(stringr)
 
-new_vector <- ifelse(str_detect(urls, "apples"), 1, 0)
-
-plot(indigenous~datePublished, BotGaz)
-plot(Indian~datePublished, BotGaz)
-str(BotGaz)
-
-BotGaz[BotGaz$Indian==1,c(1,2)]
-head(BotGaz[order(BotGaz$Native, decreasing=T),])
-head(BotGaz[order(BotGaz$indigenous, decreasing=T),])
