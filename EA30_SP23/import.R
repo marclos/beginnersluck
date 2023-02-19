ed1d76492b928dd7fa691aa39a165b8123f83ca9
marclos <marc.loshuertos@pomona.edu>
2023-02-19 13:18:05 -0800

    uddate for spring 2023 FEVB

---
+++ EA30_SP23/import.R
@@ -0,0 +1,18 @@
+## Creating Some Script
+
+### Saved as "import.R"
+
+## Getting the path/file name
+
+file.choose()
+
+## paste path to save as a vector
+
+## marc's path...
+### filepath.csv = "/home/mwl04747/beginnersluck/EA30_SP23/Field Data Recording (master sheet) - Data.csv"
+
+fileapth.csv="PASTE IN YOUR PATH/FILENAME"
+
+## read.csv
+
+import = read.csv(filepath.csv)
