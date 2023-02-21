669c06b0976e00e9d40b84fa4eb3689e1153f652
marclos <marc.loshuertos@pomona.edu>
2023-02-20 17:35:34 -0800

    test

--- EA30_SP23/Linear_Model.R
+++ EA30_SP23/Linear_Model.R
@@ -1,16 +1,24 @@
-## Lineaer Model
+## Read csv
 
+sonde.csv = "/home/mwl04747/beginnersluck/EA30_SP23/Field Data Recording (master sheet) - Data.csv"
 
-# linear model
-# some y ~ x
-# DO ~ depth
-# Temp ~ Depth
-# DO ~ Temp
+## read data into R... many statements in R are have specific vocabulary
 
-names(import)
+sonde = read.csv(sonde.csv)
 
-lm(HDO..Sat ~ Depth.m, data=import)
+## linear model
 
-DO.lm <- lm(HDO..Sat ~ Depth.m, data=import)
-summary(DO.lm)
+## lm(y~x)
 
+lm(HDO..Sat ~ Depth.m, data=sonde)
+
+DO.lm = lm(HDO..Sat ~ Depth.m, data=sonde)
+
+coef(DO.lm)
+## plot the data and add best fit line...
+
+plot(HDO..Sat ~ Depth.m, data=sonde, xlab="Depth (m)", ylab="DO (% Saturation)", 
+     las=1, main="pHake Lake Dissolved Oxygen vs. Depth")
+
+abline(coef(DO.lm), col="red")
+PlotPl
---
+++ EA30_SP23/hist.R
@@ -0,0 +1,12 @@
+## Look at central tendencies of data
+
+str(sonde)
+
+sonde$SpCond.uS.cm
+
+hist(sonde$pH.units)
+
+mean(sonde$pH.units)
+
+sd(sonde$pH.units)
+
